e3fe37c99c60ed21c4cf488b74e0d0131eaa7544
crybaby05 <benthiam750@gmail.com>
2026-06-29 23:55:21 +0000

    Ajout des fichiers de configuration de la base de données

---
+++ env.exemple.php
@@ -0,0 +1,5 @@
+<?php
+define('DB_NAME', 'ma base de donnée');
+define('DB_PASSWORD', 'mon mot de passe');
+define('DB_USER', 'mon user');
+define('DB_HOST', 'mon host');
---
+++ env.php
@@ -0,0 +1,6 @@
+<?php
+define('DB_NAME', 'gestion_articles');
+define('DB_PASSWORD', 'seydinathiam05');
+define('DB_USER', 'postgres');
+define('DB_HOST', 'localhost');
+define('DB_PORT', 5432);
