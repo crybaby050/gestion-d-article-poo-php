1dd80f59a2819675c471a6ffdc60facade466260
crybaby05 <benthiam750@gmail.com>
2026-06-29 22:30:48 +0000

    Ajout des méthodes d'accès et de modification pour la classe Article

--- entity/Article.php
+++ entity/Article.php
@@ -7,5 +7,39 @@ class Article{
     private float $prix;
     private int $quantite;
 
-    
+    public function __construct($id, $libelle, $prix, $quantite){
+
+        $this->id = $id;
+        $this->libelle = $libelle;
+        $this->prix = $prix;
+        $this->quantite = $quantite;
+    }
+
+    public function getLibelle(){
+        return $this->libelle;
+    }
+
+    public function getPrix(){
+        return $this->prix;
+    }
+
+    public function getQuantite(){
+        return $this->quantite;
+    }
+
+    public function getId(){
+        return $this->id;
+    }
+
+    public function setLibelle($libelle){
+        $this->libelle = $libelle;
+    }
+
+    public function setPrix($prix){
+        $this->prix = $prix;
+    }
+
+    public function setQuantite($quantite){
+        $this->quantite = $quantite;
+    }
 }
